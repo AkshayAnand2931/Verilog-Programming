3c9e9d6c426eb85b375e30e5da973636dc9cca5d
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2022-09-28 09:55:56 +0530

    new commit

---
+++ binarycounter.v
@@ -0,0 +1,47 @@
+`include "lib.v"
+
+module jk(clk,clear,j,k,out);
+    input wire clk,clear,j,k;
+    output wire out;
+    dfr d1(clk,clear,((j&(~out))|((~k)&out)),out);
+endmodule
+
+module counter(clk,clear,load,count,i,cout,a);
+    input wire [3:0]i;
+    input wire clk,clear,load,count;
+    output wire [3:0]a;
+    output wire cout;
+    wire w1,w2,w3,w4,w5,w6,w7,w8,w9,w10;
+
+    assign w1 = ~load;
+    and2 a1(w1,count,w2);
+
+    and2 a2(load,i[0],w3);
+    and2 a3(load,~(i[0]),w4);
+    or2 o1(w2,w3,w11);
+    or2 o2(w2,w4,w12);
+    jk j1(clk,clear,w11,w12,a[0]);
+
+    and2 a4(load,i[1],w5);
+    and2 a5(load,~(i[1]),w6);
+    and a10(w2,a[0],w13);
+    or2 o3(w13,w5,w14);
+    or2 o4(w13,w6,w15);
+    jk j2(clk,clear,w14,w15,a[1]);
+
+    and2 a6(load,i[2],w7);
+    and2 a7(load,~(i[2]),w8);
+    and2 a11(w2,(a[0]&a[1]),w16);
+    or2 o5(w7,w16,w17);
+    or2 o6(w8,w16,w18);
+    jk j3(clk,clear,w17,w18,a[2]);
+
+    and2 a8(load,i[3],w9);
+    and2 a9(load,~(i[3]),w10);
+    and a12(w2,(a[0]&a[1]&a[2]),w19);
+    or2 o7(w9,w19,w20);
+    or2 o8(w10,w19,w21);
+    jk j4(clk,clear,w20,w21,a[3]);
+
+    assign cout = (a[0]&a[1]&a[2]&a[3]);
+endmodule
--- reg_file.v
+++ reg_file.v
@@ -1,9 +1,8 @@
-// Write code for modules you need here
 `include "lib.v"
 
-module arrr(i,reset,load,clk,out);
+module arrr(clk,reset,load,i,out);
     input wire [15:0]i;
-    input wire reset,load;
+    input wire reset,load,clk;
     output wire [15:0]out;
 
     dfrl d1(clk,reset,load,i[0],out[0]);
@@ -25,20 +24,46 @@ module arrr(i,reset,load,clk,out);
 
 endmodule
 
-module arrm(i0,i1,i2,i3,i4,i5,i6,i7,addr);
+module arrm(i0,i1,i2,i3,i4,i5,i6,i7,addr,o);
     input wire [15:0]i0,i1,i2,i3,i4,i5,i6,i7;
     input wire [2:0]addr;
+    output wire [15:0]o;
 
-    demux8 d1({i0[0],i1[0],i2[0],i3[0],i4[0],i5[0],i6[0],i7[0]});
+    mux8 d1({i0[0],i1[0],i2[0],i3[0],i4[0],i5[0],i6[0],i7[0]},addr[2],addr[1],addr[0],o[0]);
+    mux8 d2({i0[1],i1[1],i2[1],i3[1],i4[1],i5[1],i6[1],i7[1]},addr[2],addr[1],addr[0],o[1]);
+    mux8 d3({i0[2],i1[2],i2[2],i3[2],i4[2],i5[2],i6[2],i7[2]},addr[2],addr[1],addr[0],o[2]);
+    mux8 d4({i0[3],i1[3],i2[3],i3[3],i4[3],i5[3],i6[3],i7[3]},addr[2],addr[1],addr[0],o[3]);
+    mux8 d5({i0[4],i1[4],i2[4],i3[4],i4[4],i5[4],i6[4],i7[4]},addr[2],addr[1],addr[0],o[4]);
+    mux8 d6({i0[5],i1[5],i2[5],i3[5],i4[5],i5[5],i6[5],i7[5]},addr[2],addr[1],addr[0],o[5]);
+    mux8 d7({i0[6],i1[6],i2[6],i3[6],i4[6],i5[6],i6[6],i7[6]},addr[2],addr[1],addr[0],o[6]);
+    mux8 d8({i0[7],i1[7],i2[7],i3[7],i4[7],i5[7],i6[7],i7[7]},addr[2],addr[1],addr[0],o[7]);
+    mux8 d9({i0[8],i1[8],i2[8],i3[8],i4[8],i5[8],i6[8],i7[8]},addr[2],addr[1],addr[0],o[8]);
+    mux8 d10({i0[9],i1[9],i2[9],i3[9],i4[9],i5[9],i6[9],i7[9]},addr[2],addr[1],addr[0],o[9]);
+    mux8 d11({i0[10],i1[10],i2[10],i3[10],i4[10],i5[10],i6[10],i7[10]},addr[2],addr[1],addr[0],o[10]);
+    mux8 d12({i0[11],i1[11],i2[11],i3[11],i4[11],i5[11],i6[11],i7[11]},addr[2],addr[1],addr[0],o[11]);
+    mux8 d13({i0[12],i1[12],i2[12],i3[12],i4[12],i5[12],i6[12],i7[12]},addr[2],addr[1],addr[0],o[12]);
+    mux8 d14({i0[13],i1[13],i2[13],i3[13],i4[13],i5[13],i6[13],i7[13]},addr[2],addr[1],addr[0],o[13]);
+    mux8 d15({i0[14],i1[14],i2[14],i3[14],i4[14],i5[14],i6[14],i7[14]},addr[2],addr[1],addr[0],o[14]);
+    mux8 d16({i0[15],i1[15],i2[15],i3[15],i4[15],i5[15],i6[15],i7[15]},addr[2],addr[1],addr[0],o[15]);
 
 endmodule
 module reg_file (input wire clk, reset, wr, input wire [2:0] rd_addr_a, rd_addr_b, wr_addr, input wire [15:0] d_in, output wire [15:0] d_out_a, d_out_b);
 
-// Declare wires here
+    wire [15:0]int0,int1,int2,int3,int4,int5,int6,int7;
+    wire [0:7]load;
 
-// Instantiate modules here
+    demux8 d1(wr,wr_addr[2],wr_addr[1],wr_addr[0],load);
+
+    arrr r1(clk,reset,load[0],d_in,int0);
+    arrr r2(clk,reset,load[1],d_in,int1);
+    arrr r3(clk,reset,load[2],d_in,int2);
+    arrr r4(clk,reset,load[3],d_in,int3);
+    arrr r5(clk,reset,load[4],d_in,int4);
+    arrr r6(clk,reset,load[5],d_in,int5);
+    arrr r7(clk,reset,load[6],d_in,int6);
+    arrr r8(clk,reset,load[7],d_in,int7);
+
+    arrm m1(int0,int1,int2,int3,int4,int5,int6,int7,rd_addr_a,d_out_a);
+    arrm m2(int0,int1,int2,int3,int4,int5,int6,int7,rd_addr_b,d_out_b);
    
 endmodule
-
-
-
